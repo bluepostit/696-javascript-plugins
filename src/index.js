69bd5f451b1c4f4aaee81d8a384d45cebc242108
Yair <32864499+bluepostit@users.noreply.github.com>
2021-09-01 22:02:22 +0300

    movie search (from prev. day)

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const movieSearch = (searchTerm) => {
+  const url = `https://www.omdbapi.com/?s=${searchTerm}&apikey=adf1f2d7`;
+  fetch(url)
+    .then(response => response.json())
+    .then((json) => {
+      const movies = json.Search;
+      console.log(`There are ${movies.length} movies found`);
+
+      // First, clear the old results!
+      const results = document.getElementById('results');
+      results.innerHTML = '';
+
+      movies.forEach((movie) => {
+        const movieTag = `<li class="list-inline-item">
+          <img src="${movie.Poster}" alt="${movie.Title}">
+          <p>${movie.Title}</p>
+        </li>`;
+        results.insertAdjacentHTML('beforeend', movieTag);
+      });
+    });
+};
+
+// Search immediately as the page loads!
+movieSearch("the matrix");
+
+
+// Movie search form
+const searchForm = document.querySelector('#movie-search');
+// Handle 'submit' event on the movie search form
+searchForm.addEventListener('submit', (event) => {
+  // Prevent page from reloading
+  event.preventDefault();
+  // Grab input from the form
+  const searchTerm = event.currentTarget.querySelector('#keyword').value;
+  console.log(`Search term: ${searchTerm}`)
+  // Build a URL to search with that input
+  // Fetch the URL
+  // Show results in the page
+  movieSearch(searchTerm);
+});
+
+
+const button = document.querySelector('#click-me');
+button.addEventListener('click', (event) => {
+  console.log('I was clicked!');
+  console.log(event.currentTarget);
+  event.currentTarget.innerText = 'Please wait...';
+  event.currentTarget.setAttribute('disabled', '');
+});
