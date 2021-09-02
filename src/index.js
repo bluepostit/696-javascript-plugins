e396e4e1b1c2c45d663a7b44c0e4a3734bdbcab7
Yair <32864499+bluepostit@users.noreply.github.com>
2021-09-02 09:19:04 +0300

    refactor: split code into different files

--- src/index.js
+++ src/index.js
@@ -1,40 +1,9 @@
-const movieSearch = (searchTerm) => {
-  const url = `https://www.omdbapi.com/?s=${searchTerm}&apikey=adf1f2d7`;
-  fetch(url)
-    .then(response => response.json())
-    .then((json) => {
-      const movies = json.Search;
-      console.log(`There are ${movies.length} movies found`);
-
-      // First, clear the old results!
-      const results = document.getElementById('results');
-      results.innerHTML = '';
-
-      movies.forEach((movie) => {
-        const movieTag = `<li class="list-inline-item">
-          <img src="${movie.Poster}" alt="${movie.Title}">
-          <p>${movie.Title}</p>
-        </li>`;
-        results.insertAdjacentHTML('beforeend', movieTag);
-      });
-    });
-};
+import { movieSearch, onSearch } from './movies';
 
 // Search immediately as the page loads!
 movieSearch("the matrix");
 
-
 // Movie search form
 const searchForm = document.querySelector('#movie-search');
 // Handle 'submit' event on the movie search form
-searchForm.addEventListener('submit', (event) => {
-  // Prevent page from reloading
-  event.preventDefault();
-  // Grab input from the form
-  const searchTerm = event.currentTarget.querySelector('#keyword').value;
-  console.log(`Search term: ${searchTerm}`)
-  // Build a URL to search with that input
-  // Fetch the URL
-  // Show results in the page
-  movieSearch(searchTerm);
-});
+searchForm.addEventListener('submit', onSearch);
---
+++ src/movies.js
@@ -0,0 +1,39 @@
+const insertMovies = (movies) => {
+  // First, clear the old results!
+  const results = document.getElementById('results');
+  results.innerHTML = '';
+
+  movies.forEach((movie) => {
+    const movieTag = `<li class="list-inline-item">
+      <img src="${movie.Poster}" alt="${movie.Title}">
+      <p>${movie.Title}</p>
+    </li>`;
+    results.insertAdjacentHTML('beforeend', movieTag);
+  });
+};
+
+const movieSearch = (searchTerm) => {
+  const url = `https://www.omdbapi.com/?s=${searchTerm}&apikey=adf1f2d7`;
+  fetch(url)
+    .then(response => response.json())
+    .then((json) => {
+      const movies = json.Search;
+      console.log(`There are ${movies.length} movies found`);
+
+      insertMovies(movies);
+    });
+};
+
+const onSearch = (event) => {
+  // Prevent page from reloading
+  event.preventDefault();
+  // Grab input from the form
+  const searchTerm = event.currentTarget.querySelector('#keyword').value;
+  console.log(`Search term: ${searchTerm}`);
+  // Build a URL to search with that input
+  // Fetch the URL
+  // Show results in the page
+  movieSearch(searchTerm);
+}
+
+export { movieSearch, onSearch };
